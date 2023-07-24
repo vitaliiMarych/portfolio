c24fa74b52fdc1e3e8509548567ce538cad52952
Vitalii Marych <apiskin557@gmail.com>
2023-07-24 11:24:07 +0300

    Added quizHub project

--- JS/index.js
+++ JS/index.js
@@ -4,11 +4,8 @@ var menu = document.querySelector('.header-links');
 var mobileMenu = document.querySelector('.header-links-active');
 
 menuToggle.addEventListener('click', function() {
-  // Перевіряємо, чи активований згорнутий список
-  console.log("fgrfsef");
   var isActive = menu.classList.contains('active');
 
-  // Змінюємо стан згорнутого списку та значку кнопки переключення
   if (isActive) {
     menu.classList.remove('active');
     menuToggle.classList.remove('active');
